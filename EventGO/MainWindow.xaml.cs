095ff728b583fb49d1e709c9d9ad0318f20826c9
俊凯 丁 <h2722877@live.com>
2019-10-31 17:59:53 +0800

    event

---
+++ EventGO/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventGO
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ReportTimeEventArgs : RoutedEventArgs
+    {
+        public ReportTimeEventArgs(RoutedEvent e, object source) : base(e, source) { }
+
+        public DateTime ClickTime { get; set; }
+
+    }
+
+    public class TimeButton : Button
+    {
+        public static readonly RoutedEvent ReportTimeEvent = EventManager.RegisterRoutedEvent
+            ("ReportTime", RoutingStrategy.Bubble, typeof(EventHandler<ReportTimeEventArgs>), typeof(TimeButton));
+        public event RoutedEventHandler ReportTime
+        {
+            add
+            {
+                AddHandler(ReportTimeEvent, value);
+            }
+            remove
+            {
+                RemoveHandler(ReportTimeEvent, value);
+            }
+        }
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+            ReportTimeEventArgs reportTimeEventArgs = new ReportTimeEventArgs(ReportTimeEvent, this) { ClickTime = DateTime.Now };
+            ReportTime += TimeButton_ReportTime;
+            RaiseEvent(reportTimeEventArgs);
+        }
+
+        private void TimeButton_ReportTime(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(((ReportTimeEventArgs)e).ClickTime.ToString());
+        }
+    }
+}
