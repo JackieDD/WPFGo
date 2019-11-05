d0bb638c76a7923fd2cd53df4ffa6fc7faa5559e
俊凯 丁 <h2722877@live.com>
2019-11-05 17:08:23 +0800

    template

---
+++ TemplateGo/DataGridWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TemplateGo
+{
+    /// <summary>
+    /// DataGridWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataGridWindow : Window
+    {
+        public DataGridWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = e.OriginalSource as TextBox;
+            ContentPresenter contentPresenter = tb.TemplatedParent as ContentPresenter;
+                
+        }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Skill { get; set; }
+        public bool HasJob { get; set; }
+    }
+}
--- TemplateGo/MainWindow.xaml.cs
+++ TemplateGo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,13 @@ namespace TemplateGo
         public MainWindow()
         {
             InitializeComponent();
-            listBox.Items.Add(1);
+            //listBox.Items.Add(1);
         }
+    }
+
+    public class Unit
+    {
+        public int Price { get; set; }
+        public string Year { get; set; }
     }
 }
